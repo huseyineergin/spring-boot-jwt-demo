4e7746cfbcb6231687780bac63f8037cc3ff39d2
Hüseyin Emre Ergin <100281013+huseyineergin@users.noreply.github.com>
2025-07-01 00:04:06 +0300

    handle invalid token

--- server/src/main/java/com/example/server/expection/GlobalExpectionHandler.java
+++ server/src/main/java/com/example/server/expection/GlobalExpectionHandler.java
@@ -9,6 +9,12 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 @RestControllerAdvice
 public class GlobalExpectionHandler {
 
+  @ExceptionHandler(TokenInvalidException.class)
+  public ResponseEntity<ErrorResponse> handleTokenInvalidException(TokenInvalidException ex) {
+    ErrorResponse error = new ErrorResponse(HttpStatus.UNAUTHORIZED.value(), ex.getMessage());
+    return new ResponseEntity<>(error, HttpStatus.UNAUTHORIZED);
+  }
+
   @ExceptionHandler(UsernameTakenException.class)
   public ResponseEntity<ErrorResponse> handleUsernameTakenException(UsernameTakenException ex) {
     ErrorResponse error = new ErrorResponse(HttpStatus.CONFLICT.value(), ex.getMessage());
--- server/src/main/java/com/example/server/filter/JwtFilter.java
+++ server/src/main/java/com/example/server/filter/JwtFilter.java
@@ -8,9 +8,12 @@ import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
+import org.springframework.web.servlet.HandlerExceptionResolver;
 
+import com.example.server.expection.TokenInvalidException;
 import com.example.server.service.JwtService;
 import com.example.server.service.UserService;
+import com.example.server.service.UserTokenService;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
@@ -24,31 +27,43 @@ public class JwtFilter extends OncePerRequestFilter {
 
   private final JwtService jwtService;
   private final UserService userService;
+  private final UserTokenService userTokenService;
+  private final HandlerExceptionResolver handlerExceptionResolver;
 
   @Override
   protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain)
       throws ServletException, IOException {
-    final String authHeader = request.getHeader("Authorization");
+    try {
+      final String authHeader = request.getHeader("Authorization");
 
-    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+      if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+        filterChain.doFilter(request, response);
+        return;
+      }
+
+      final String token = authHeader.substring(7);
+
+      if (!userTokenService.isTokenActive(token)) {
+        throw new TokenInvalidException("Invalid token.");
+      }
+
+      final String username = jwtService.extractUsername(token);
+      Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+
+      if (username != null && authentication == null) {
+        UserDetails userDetails = userService.loadUserByUsername(username);
+
+        if (jwtService.validateToken(token, userDetails.getUsername())) {
+          UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+              userDetails, null, userDetails.getAuthorities());
+          SecurityContextHolder.getContext().setAuthentication(authToken);
+        }
+      }
       filterChain.doFilter(request, response);
+    } catch (Exception ex) {
+      handlerExceptionResolver.resolveException(request, response, null, ex);
       return;
     }
-
-    final String token = authHeader.substring(7);
-    final String username = jwtService.extractUsername(token);
-    Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-    if (username != null && authentication == null) {
-      UserDetails userDetails = userService.loadUserByUsername(username);
-
-      if (jwtService.validateToken(token, userDetails.getUsername())) {
-        UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
-            userDetails, null, userDetails.getAuthorities());
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-      }
-    }
-    filterChain.doFilter(request, response);
   }
 
 }
